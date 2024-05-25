7dc48ba885e9ef80a531c70996c5ff2f966f06b8
athika98 <athika@abira.ch>
2024-05-25 10:58:38 +0200

    streamlit_app.py angepasst und datensatz hinzugefügt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,25 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-st.set_page_config(page_title="Hello Streamlit")
-st.header("My app");
-st.write("Hello World.")
+# Daten laden
+@st.cache_data  # Use cache_data for caching data
+def load_data():
+    data = pd.read_csv('bfs_data.csv', delimiter=';')  # Semikolon als Trennzeichen
+    return data
 
-st.write("Here's our first attempt at using data to create a table:") 
-st.write(pd.DataFrame({ 
-    'first column': [1, 2, 3, 4], 
-    'second column': [10, 20, 30, 40] 
-})) 
+data = load_data()
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3), 
-    columns=['a', 'b', 'c']
-) 
-st.line_chart(chart_data)
+# App-Titel und Beschreibung
+st.title('Bevölkerungsstatistik der Stadt Zürich')
+st.write("""
+Diese App ermöglicht die Visualisierung der Bevölkerungsdaten von Zürich nach Postleitzahl und Jahr.
+Wählen Sie ein Jahr aus, um die Bevölkerungsdaten für alle Züricher Postleitzahlen zu sehen.
+""")
 
+# Benutzerinterface
+year_to_filter = st.radio('Jahr wählen', options=data['Jahr'].unique())
+filtered_data = data[data['Jahr'] == year_to_filter]
+
+# Daten visualisieren
+st.write(f"Bevölkerungsdaten für das Jahr {year_to_filter}")
+st.bar_chart(filtered_data[['Postleitzahl', 'Total']].set_index('Postleitzahl'))
